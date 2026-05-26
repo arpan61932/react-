8db968d9ed84d56487e32254e580cccd868f2cc1
arpan61932 <arpanmaji619@gmail.com>
2026-05-26 10:52:13 +0530

    first commit

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <h2>Welcome to your React app</h2>
+    </div>
+  )
+}
+
+export default App
+
