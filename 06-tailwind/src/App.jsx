26681f98df30bc05a85506114a1586458cb286c5
arpan61932 <arpanmaji619@gmail.com>
2026-06-14 08:21:21 +0530

    tailwind

---
+++ 06-tailwind/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-gray-300 h-screen w-full'>
+      hello world kaise ho aap log
+    </div>
+  )
+}
+
+export default App
